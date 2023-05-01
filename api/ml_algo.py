658cd066b998e5eb6e3af0a50a542266bdf3ee51
yashzod <yashzod748@gmail.com>
2023-05-01 23:01:36 +0530

    temp

--- api/ml_algo.py
+++ api/ml_algo.py
@@ -1,13 +1,43 @@
 
 from sklearn.linear_model import LinearRegression
 
+{
+    "model":"linearregression",
+    "file_name":"file_name",
+    "train_test_split":{
+        "train":70,
+        "test":20,
+    },
+    "column_processing":[
+        {
+            "column_name1":{
+                "missing_value":"average",
+                "encoding":"one_hot",
+                "feature_scaling":"0-1"
+            },
+            "column_name2":{
+                "missing_value":"average",
+                "encoding":"one_hot",
+                "feature_scaling":"-1-1"
+            }
+        }
+    ],
+}
 
-def linear_regression(df,x_cols,y_col,train_test_split,):
-    x = df[x_cols]
-    y = df[y_col]
+def linear_regression(df,x_cols,y_col,train_test_split):
 
-    reg = LinearRegression().fit(x, y)
+    dataX = df[x_cols]
+    dataY = df[y_col]
 
-    score = reg.score(x, y)
+    train_ratio = train_test_split['train']/100
+    test_ratio = train_test_split['test']/100
 
-    return score
+    x_train, x_test, y_train, y_test = train_test_split(dataX, dataY, test_size=test_ratio)
+    
+    reg = LinearRegression().fit(x_train, y_train)
+
+    test_score = reg.score(x_test, y_test)
+
+    return test_score
+    
+
---
+++ api/train.py
@@ -0,0 +1,8 @@
+from .ml_algo import linear_regression
+
+
+model_mapping = {
+    'linearregression':linear_regression
+}
+
+
