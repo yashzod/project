92d9ad4ce7d37c6da9fc3db54781aa646f3fd75e
yashzod <yashzod748@gmail.com>
2023-05-21 13:17:05 +0530

    plot

--- api/ml_algo.py
+++ api/ml_algo.py
@@ -1,5 +1,14 @@
 
 from sklearn.linear_model import LinearRegression
+import sklearn.model_selection
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import PolynomialFeatures
+from mlxtend.evaluate import bias_variance_decomp
 
 {
     "model":"linearregression",
@@ -32,12 +41,43 @@ def linear_regression(df,x_cols,y_col,train_test_split):
     train_ratio = train_test_split['train']/100
     test_ratio = train_test_split['test']/100
 
-    x_train, x_test, y_train, y_test = train_test_split(dataX, dataY, test_size=test_ratio)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(dataX, dataY, test_size=test_ratio)
     
-    reg = LinearRegression().fit(x_train, y_train)
+    # reg = LinearRegression().fit(x_train, y_train)
 
-    test_score = reg.score(x_test, y_test)
+    # test_score = reg.score(x_test, y_test)
 
-    return test_score
+    degrees = list(range(10))
+
+    train_errors, test_errors, r2_scores, bias, variance = [], [], [], [], []
+
+    for degree in degrees:
+        poly_features = PolynomialFeatures(degree=degree)
+        X_poly_train = poly_features.fit_transform(X_train)
+        X_poly_test = poly_features.transform(X_test)
+        model = LinearRegression()
+        model.fit(X_poly_train, y_train)
+        train_pred = model.predict(X_poly_train)
+        test_pred = model.predict(X_poly_test)
+        train_error = mean_squared_error(y_train, train_pred)
+        test_error = mean_squared_error(y_test, test_pred)
+        train_errors.append(train_error)
+        test_errors.append(test_error)
+        r2_scores.append(r2_score(y_test, test_pred))
+        # this_bias, this_var, _ = bias_variance_decomp(model, X_poly_train, y_train, X_poly_test, y_test, loss='mse', num_rounds=200, random_seed=1)
+        # bias.append(this_bias)
+        # variance.append(this_var)
+
+    data = []
+
+    data.append([
+        {
+            "x":degrees,
+            "train errors":train_errors,
+            "test errors":test_errors
+        }
+    ])
+
+    return data
     
 
--- api/views.py
+++ api/views.py
@@ -8,22 +8,35 @@ import pandas as pd
 import io
 from time import sleep
 from .helper import process_df
+from django.shortcuts import redirect
+from .ml_algo import linear_regression
 # Create your views here.
 
 class UploadFileView(APIView):
 
+    def get(self, request):
+        data = request.body.decode("utf-8")
+        print(data,"******************************************")
+        return Response({'msg':'uploaded'},status=status.HTTP_200_OK)
+
+
     def post(self,request):
+        print("*******************************************",request.data)
+
         serializer = CsvFileSerializer(data={'file':request.data['file']})
+        print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&",request.data['file'])
         if serializer.is_valid():
             path =  default_storage.save('file.csv',request.data['file'])
-
+            print(path)
             serializer = CsvFilePathSerializer(data={'path':path})
             if serializer.is_valid():
                 serializer.save()
             df = pd.read_csv(io.StringIO(default_storage.open(path).read().decode('utf-8')))
             columns = df.columns
+            # return redirect('https://s6e2u5.csb.app/')
             return Response({'msg':'uploaded','path':path,'columns':columns},status=status.HTTP_200_OK)
-        return Response({'msg':'not uploaded','error':serializer.error_messages},status=status.HTTP_404_NOT_FOUND)
+        print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^',serializer.error_messages)
+        return Response({'msg':'not uploaded','error':serializer.error_messages},status=status.HTTP_400_BAD_REQUEST)
 
 class AttributesView(APIView):
 
@@ -92,19 +105,54 @@ class AttributesView(APIView):
 class TrainModels(APIView):
 
     def post(self,request):
+        print(request.data)
+        data = [
+                [
+                    {
+                    "x": [1, 2, 3, 4, 5],
+                    "y1": [1, 3, 2, 4, 5],
+                    "y2": [2, 4, 3, 5, 6],
+                    "y3": [3, 5, 4, 6, 7]
+                    },
+                    {
+                    "x": [1, 2, 3, 4, 5],
+                    "y1": [5, 4, 3, 2, 1],
+                    "y2": [6, 5, 4, 3, 2],
+                    "y3": [7, 6, 5, 4, 3]
+                    }
+                ],
+                [
+                    {
+                    "x": [1, 2, 3, 4, 5],
+                    "y1": [10, 9, 8, 7, 6],
+                    "y2": [11, 10, 9, 8, 7],
+                    "y3": [12, 11, 10, 9, 8]
+                    },
+                    {
+                    "x": [1, 2, 3, 4, 5],
+                    "y1": [6, 7, 8, 9, 10],
+                    "y2": [7, 8, 9, 10, 11],
+                    "y3": [8, 9, 10, 11, 12]
+                    }
+                ]
+                ]
+        # return Response({"data":data},status=status.HTTP_200_OK)
+
         model = request.data['model']
-        train_split = request.data['train_test_split']['train']
-        test_split = request.data['train_test_split']['test']
-        validation_split = request.data['train_test_split']['validation']
+        # train_split = request.data['train_test_split']['train']
+        # test_split = request.data['train_test_split']['test']
+        # validation_split = request.data['train_test_split']['validation']
         file_name = request.data['file_name']
         df = pd.read_csv(io.StringIO(default_storage.open(file_name).read().decode('utf-8')))
-        column_processing = request.data['column_processing']
-        processed_df = process_df(df,column_processing)
-        print(df)
-        testt = request.data['column_processing'][0]['column_name1']['missing_value']
+        # column_processing = request.data['column_processing']
+        # processed_df = process_df(df,column_processing)
+        x_cols = ["a",'b','c','d']
+        y_col = "e"
+        data = linear_regression(df,train_test_split=request.data['train_test_split'],x_cols=x_cols,y_col=y_col)
+        print(data)
 
-        print(model,file_name,validation_split,testt)
-        return Response({'msg':'success'},status=status.HTTP_200_OK)
+
+        return Response({"data":data},status=status.HTTP_200_OK)
 
 
 {
@@ -131,3 +179,4 @@ class TrainModels(APIView):
     ],
     "validation_type":"K-fold"
 }
+
